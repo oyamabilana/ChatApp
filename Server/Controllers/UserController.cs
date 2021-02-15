42b78c4cc6daafc470934fd779af845fdd12493d
Oyama <34276719+oyamabilana@users.noreply.github.com>
2021-02-15 15:01:08 +0200

    first commit

---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ChatApp.Shared;
+using ChatApp.Server.Models;
+
+namespace ChatApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        private readonly ChatAppContext _context;
+
+        public UserController(ILogger<UserController> logger, ChatAppContext context)
+        {
+            _logger = logger;
+            this._context = context;
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
